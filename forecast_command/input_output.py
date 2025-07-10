7ac75e402cc6b7b0d55393652924a15035df35c7
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-07-10 17:50:12 -0400

    Consolidate retrieve_url_from_zip() and print_forecast() into ForecastLoop class

--- forecast_command/input_output.py
+++ forecast_command/input_output.py
@@ -47,6 +47,14 @@ class ForecastLoop:
         """
         print_wrapped(ENTER_ZIP_PROMPT)
 
+    def fahrenheit(self) -> None:
+        """Run the input loop for printing forecasts in Fahrenheit."""
+        self._process_zip_input(TempScale.FAHRENHEIT)
+
+    def celsius(self) -> None:
+        """Run the input loop for printing forecasts in Celsius."""
+        self._process_zip_input(TempScale.CELSIUS)
+
     def _process_zip_input(self, temp_scale: TempScale) -> None:
         """
         Prompt the user to enter a zip code, print the forecast for
@@ -58,29 +66,74 @@ class ForecastLoop:
         """
         # While loop to deploy functions and get input from the user
         while True:
-            url: str | None = retrieve_url_from_zip(temp_scale)
+            url: str | None = self._retrieve_url_from_zip(temp_scale)
             if url is None:
                 break
-            print_forecast(url)
+            self._print_forecast(url)
             print_wrapped(ANY_OTHER_ZIP_PROMPT)
 
-    def fahrenheit(self) -> None:
+    def _retrieve_url_from_zip(self, temp_scale: TempScale) -> str | None:
         """
-        Run the input loop for printing forecasts in Fahrenheit.
+        Request a valid zip code that matches a zip code in the JSON file
+        and return the matching URL.
 
         Args:
-            self: The instance of the ForecastLoop class.
-        """
-        self._process_zip_input(TempScale.FAHRENHEIT)
+            temp_scale: The temperature scale to apply to the forecast.
 
-    def celsius(self) -> None:
+        Returns:
+            url: The url for the zip code input.
+            None: If the user signals to exit.
         """
-        Run the input loop for printing forecasts in Celsius.
+        while True:
+            zip_code_input: str = input().strip().lower()
+
+            if zip_code_input in (NO_INPUTS | EXIT_INPUTS):
+                return None
+            elif zip_code_input in YES_INPUTS:
+                print_wrapped(ENTER_VALID_ZIP_PROMPT)
+                continue
+
+            try:
+                validate_zip_code(zip_code_input)
+                url = zip_codes_dict[zip_code_input]
+                if temp_scale == TempScale.CELSIUS:
+                    url += CELSIUS_URL_SUFFIX
+                validate_url(url)
+                return url
+            except (
+                NoZipCodeError,
+                InvalidZipCodeFormatError
+            ) as e:
+                print_wrapped(str(e))
+                print_wrapped(ENTER_VALID_ZIP_PROMPT)
+            except (
+                ZipCodeNotFoundError,
+                NoDataForZipCodeError,
+                InvalidUrlFormatError
+            ) as e:
+                print_wrapped(str(e))
+                print_wrapped(ANY_OTHER_ZIP_PROMPT)
+
+    def _print_forecast(self, url: str) -> None:
+        """
+        Print forecast data from a given URL to the console.
 
         Args:
-            self: The instance of the ForecastLoop class.
+            url: The URL for accessing weather data.
         """
-        self._process_zip_input(TempScale.CELSIUS)
+        try:
+            day_forecasts: list[str] = parse_forecast(url)
+            formatted_forecasts: list[str] = format_forecasts(day_forecasts)
+            for day_forecast in formatted_forecasts:
+                print_wrapped(day_forecast)
+        except requests.exceptions.ConnectionError:
+            print_wrapped('No internet connection. Please try again.')
+        except requests.exceptions.Timeout:
+            print_wrapped('The request timed out. Please try again.')
+        except HTMLElementNotFoundError as e:
+            print_wrapped(f'HTML element not found: {e}')
+        except Exception as e:
+            print_wrapped(f'An unexpected error occurred: {e}')
 
 
 def print_padding() -> None:
@@ -129,68 +182,3 @@ def prompt_for_temp_scale() -> str:
                 print_wrapped(ENTER_VALID_TEMP_SCALE_PROMPT)
             else:
                 return temp_scale_input
-
-
-def retrieve_url_from_zip(temp_scale: TempScale) -> str:
-    """
-    Request a valid zip code that matches a zip code in the JSON file
-    and return the matching URL.
-
-    Args:
-        temp_scale: The temperature scale to apply to the forecast.
-
-    Returns:
-        url: The url for the zip code input.
-        None: If the user signals to exit.
-    """
-    while True:
-        zip_code_input: str = input().strip().lower()
-
-        if zip_code_input in (NO_INPUTS | EXIT_INPUTS):
-            return None
-        elif zip_code_input in YES_INPUTS:
-            print_wrapped(ENTER_VALID_ZIP_PROMPT)
-            continue
-
-        try:
-            validate_zip_code(zip_code_input)
-            url = zip_codes_dict[zip_code_input]
-            if temp_scale == TempScale.CELSIUS:
-                url += CELSIUS_URL_SUFFIX
-            validate_url(url)
-            return url
-        except (
-            NoZipCodeError,
-            InvalidZipCodeFormatError
-        ) as e:
-            print_wrapped(str(e))
-            print_wrapped(ENTER_VALID_ZIP_PROMPT)
-        except (
-            ZipCodeNotFoundError,
-            NoDataForZipCodeError,
-            InvalidUrlFormatError
-        ) as e:
-            print_wrapped(str(e))
-            print_wrapped(ANY_OTHER_ZIP_PROMPT)
-
-
-def print_forecast(url: str) -> None:
-    """
-    Print forecast data from a given URL to the console.
-
-    Args:
-        url: The URL for accessing weather data.
-    """
-    try:
-        day_forecasts: list[str] = parse_forecast(url)
-        formatted_forecasts: list[str] = format_forecasts(day_forecasts)
-        for day_forecast in formatted_forecasts:
-            print_wrapped(day_forecast)
-    except requests.exceptions.ConnectionError:
-        print_wrapped('No internet connection. Please try again.')
-    except requests.exceptions.Timeout:
-        print_wrapped('The request timed out. Please try again.')
-    except HTMLElementNotFoundError as e:
-        print_wrapped(f'HTML element not found: {e}')
-    except Exception as e:
-        print_wrapped(f'An unexpected error occurred: {e}')
