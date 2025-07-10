6f3e2bfb91b55ce79461a1d2768acdf8e29caae5
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-07-10 02:24:21 -0400

    Remove exception classes

--- forecast_command/validation.py
+++ forecast_command/validation.py
@@ -3,45 +3,16 @@ from forecast_command.constants import (
     CELSIUS_INPUTS,
     FAHRENHEIT_INPUTS
 )
+from forecast_command.exceptions import (
+    InvalidTempScaleError,
+    InvalidUrlFormatError,
+    InvalidZipCodeFormatError,
+    NoDataForZipCodeError,
+    NoTempScaleError,
+    NoZipCodeError,
+    ZipCodeNotFoundError
+)
 from forecast_command.regexes import ValidationRegexes
-
-
-class HTMLElementNotFoundError(Exception):
-    """Exception raised when a given HTML element cannot be found."""
-
-
-class InvalidTempScaleError(Exception):
-    """Exception raised when the provided temperature scale string is
-    invalid."""
-
-
-class InvalidUrlFormatError(Exception):
-    """Exception raised when the provided string does not match
-    weather.gov's forecast URL syntax."""
-
-
-class InvalidZipCodeFormatError(Exception):
-    """Exception raised when the provided string is not a valid zip
-    code."""
-
-
-class NoTempScaleError(Exception):
-    """Exception raised when the provided temperature scale string is
-    empty."""
-
-
-class NoDataForZipCodeError(Exception):
-    """Exception raised when there is no data available for the given
-    zip code."""
-
-
-class NoZipCodeError(Exception):
-    """Exception raised when the provided string is empty."""
-
-
-class ZipCodeNotFoundError(Exception):
-    """Exception raised when the zip code string is not found in the
-    given JSON file."""
 
 
 def validate_temp_scale(temp_scale_input: str) -> None:
