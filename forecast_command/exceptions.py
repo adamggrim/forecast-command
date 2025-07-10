fea90a88baa0127ed7a34527fe64a5bc79d17052
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-07-10 05:01:02 -0400

    Implement base class inheritance for exceptions

--- forecast_command/exceptions.py
+++ forecast_command/exceptions.py
@@ -1,36 +1,46 @@
-class HTMLElementNotFoundError(Exception):
+# --- Data Errors ---
+class DataError(Exception):
+    """Base class for data errors."""
+
+
+class HTMLElementNotFoundError(DataError):
     """Exception raised when a given HTML element cannot be found."""
 
 
-class InvalidTempScaleError(Exception):
-    """Exception raised when the provided temperature scale string is
-    invalid."""
-
-
-class InvalidUrlFormatError(Exception):
-    """Exception raised when the provided string does not match
-    weather.gov's forecast URL syntax."""
-
-
-class InvalidZipCodeFormatError(Exception):
-    """Exception raised when the provided string is not a valid zip
-    code."""
-
-
-class NoTempScaleError(Exception):
-    """Exception raised when the provided temperature scale string is
-    empty."""
-
-
-class NoDataForZipCodeError(Exception):
+class NoDataForZipCodeError(DataError):
     """Exception raised when there is no data available for the given
     zip code."""
 
 
-class NoZipCodeError(Exception):
-    """Exception raised when the provided string is empty."""
-
-
-class ZipCodeNotFoundError(Exception):
+class ZipCodeNotFoundError(DataError):
     """Exception raised when the zip code string is not found in the
     given JSON file."""
+
+
+# --- Input Errors ---
+class InputError(Exception):
+    """Base class for input errors."""
+
+
+class InvalidTempScaleError(InputError):
+    """Exception raised when the provided temperature scale string is
+    invalid."""
+
+
+class InvalidUrlFormatError(InputError):
+    """Exception raised when the provided string does not match
+    weather.gov's forecast URL syntax."""
+
+
+class InvalidZipCodeFormatError(InputError):
+    """Exception raised when the provided string is not a valid zip
+    code."""
+
+
+class NoTempScaleError(InputError):
+    """Exception raised when the provided temperature scale string is
+    empty."""
+
+
+class NoZipCodeError(InputError):
+    """Exception raised when the provided string is empty."""
