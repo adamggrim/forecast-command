6ce4418e11e070e191d558f823fc4c02b3024d0f
adamggrim <97310482+adamggrim@users.noreply.github.com>
2025-07-15 20:07:08 -0400

    Update constants.py documentation

--- forecast_command/constants.py
+++ forecast_command/constants.py
@@ -3,58 +3,58 @@ class HelpMessages:
     Help message strings for command-line arguments.
 
     Attributes:
-        DESCRIPTION: Description for temperature scale arguments
+        DESCRIPTION: Description for temperature scale arguments.
 
-        CELSIUS: Help message for -c/--celsius argument
-        FAHRENHEIT: Help message for -f/--fahrenheit argument
+        CELSIUS: Help message for -c/--celsius argument.
+        FAHRENHEIT: Help message for -f/--fahrenheit argument.
     """
     DESCRIPTION: str = 'Make an optional specification for temperature scale.'
 
     CELSIUS: str = 'get the forecast in Celsius'
     FAHRENHEIT: str = 'get the forecast in Fahrenheit'
 
-# String printed to prompt the user for another zip code
+# Prompt for the user for another zip code.
 ANY_OTHER_ZIP_PROMPT: str = 'Any other zip code? (5 digits):'
 
-# Set of strings for selecting Celsius
+# Inputs for selecting Celsius.
 CELSIUS_INPUTS: set[str] = {'celsius', 'c'}
 
-# String appended to a URL for Celsius forecasts
+# Suffix appended to a URL for Celsius forecasts.
 CELSIUS_URL_SUFFIX: str = '&FcstType=text&unit=1'
 
-# Name for the directory containing data files
+# Name for the directory containing data files.
 DATA_DIR_NAME: str = 'data'
 
-# String printed to prompt the user to enter a temperature scale
+# Prompt for the user to enter a temperature scale.
 ENTER_TEMP_SCALE_PROMPT: str = (
     'Enter a temperature scale (Celsius [C] or Fahrenheit [F]):'
 )
 
-# String printed when the previous temperature scale input was invalid
+# Prompt for when the previous temperature scale input was invalid.
 ENTER_VALID_TEMP_SCALE_PROMPT: str = (
     'Please enter Celsius (C) or Fahrenheit (F):'
 )
 
-# String printed when the previous zip code input was invalid
+# Prompt for when the previous zip code input was invalid.
 ENTER_VALID_ZIP_PROMPT: str = 'Please enter a valid zip code:'
 
-# String printed to prompt the user to enter a zip code
+# Prompt for the user to enter a zip code.
 ENTER_ZIP_PROMPT: str = 'Enter zip code (5 digits):'
 
-# Set of strings for exiting the program
+# Inputs for exiting the program.
 EXIT_INPUTS: set[str] = {'quit', 'q', 'exit', 'e'}
 
-# String printed when the user exits the program
+# Message for when the user exits the program.
 EXIT_MESSAGE: str = 'Exiting the program...'
 
-# Set of strings for selecting Fahrenheit
-FAHRENHEIT_INPUTS: str = {'fahrenheit', 'f'}
+# Inputs for selecting Fahrenheit.
+FAHRENHEIT_INPUTS: set[str] = {'fahrenheit', 'f'}
 
-# Set of strings for indicating a negative response
+# Inputs for indicating a negative response.
 NO_INPUTS: set[str] = {'no', 'n'}
 
-# Set of strings for indicating an affirmative response
+# Inputs for indicating an affirmative response.
 YES_INPUTS: set[str] = {'yes', 'y'}
 
-# String for the file name of the zip code to URL map.
+# The file name of the zip code to URL map.
 ZIP_CODE_MAP_FILE_NAME: str = 'zip_code_to_url_map.json'
